55e7fcc2ace50c13b6f91632de35325cfbf007c5
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2023-01-17 15:07:46 +0100

    :javascript

--- functions.js
+++
@@ -1,8 +0,0 @@
-export const randomString = () => {
-    const str = "qwertyuiopasdfghjklzxcvbnm";
-    let newStr = "";
-    for(let i = 0; i < 7; i++) {
-        newStr += str[Math.floor(Math.random() * 7)]
-    }
-    return newStr;
-}
