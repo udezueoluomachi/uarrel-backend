9d441d1c4dc53331f969d25a618685c76d9c1724
udezueoluomachi <74429592+udezueoluomachi@users.noreply.github.com>
2023-01-18 10:27:39 +0100

    fixed issue with email

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This is the backed server for my URL shortener : **[UAREL](https://github.com/udezueoluomachi/uarel)**
 
 _still in development phase_
+
+# INFO
+G Password is from my google account settings after enabling 2FA and creating a uniques password for this app
--- functions.js
+++ functions.js
@@ -1,10 +1,16 @@
 import {config} from "dotenv";
 import fs from "fs";
+import notifier from "mail-notifier";
 import puppeteer from "puppeteer-core";
 config();
 
 const _env = process.env;
 const chromePath = _env.CHROME_PATH;
+
+const clientId = _env.CLIENT_ID;
+const clientSecret = _env.CLIENT_SECRET;
+const refreshToken = _env.REFRESH_TOKEN;
+
 
 const randomString = () => {
     const str = "qwertyuiopasdfghjklzxcvbnm";
@@ -47,7 +53,34 @@ const deleteFile = path => {
     })
 }
 
-//const pushToGithub = path => {
+const extractVerificationCode = str => {
+    const pattern = /\d+/igm;
+    return str.match(pattern)[0];
+}
+
+const listenMail = (callback) => {
+    const imap = {
+        user: _env.EMAIL,
+        password : _env.G_PASS,
+        host: "imap.gmail.com",
+        port: 993,
+        tls: true,
+        searchFilter: ["NEW", "UNSEEN", ["SINCE", new Date().toUTCString()]],
+          tlsOptions: { rejectUnauthorized: false }
+    };
+    
+    notifier(imap).on('mail',function(mail){
+      if(mail.from[0].address == "noreply@github.com") {
+        callback(extractVerificationCode(mail.text));
+      }
+    }).start()
+}
+
+//check if verification page before verifying 
+
+//listenMail();
+
+const pushToGithub = async path => {
     let launchOptions = {  headless : false,
         executablePath: chromePath,
         args: ['--start-maximized']
@@ -69,6 +102,11 @@ const deleteFile = path => {
 
     await page.goto("https://github.com/login");
 
-    await page.type("#login_field",_env.EMAIL, {delay : 4});
-    await page.type("#password",_env.PASS)
-//}
+    await page.type("#login_field", _env.EMAIL, {delay : 4});
+    await page.type("#password", _env.PASS, {delay : 3});
+
+    await page.click("input[type='submit']", {delay : 2});
+
+    //await browser.close();
+}
+//pushToGithub();
